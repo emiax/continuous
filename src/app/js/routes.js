8e048876b92372b73cb1ecbf973ed8c45e77c893
J-Zeitler <jonze168@student.liu.se>
2013-10-15 12:10:56 +0200

    added src dir with angular and bower

---
+++ src/app/js/routes.js
@@ -0,0 +1,17 @@
+define(['angular', 'app'], function(angular, app) {
+	'use strict';
+
+	return app.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/visualization', {
+			templateUrl: 'app/partials/visualization.html',
+			controller: 'VisualizationCtrl'
+		});
+
+		$routeProvider.when('/view1', {
+			templateUrl: 'app/partials/partial1.html',
+			controller: 'MyCtrl1'
+		});
+		//$routeProvider.otherwise({redirectTo: '/view1'});
+	}]);
+
+});
