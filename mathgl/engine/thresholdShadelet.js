9847b4d11f635bc763a83a4c578f300d49505a92
emiax775 <emiax775@student.liu.se>
2013-10-27 21:13:26 +0100

    threshold shadelet

---
+++ mathgl/engine/thresholdShadelet.js
@@ -0,0 +1,39 @@
+define(['quack', 'mathgl/engine/shadelet.js', 'mathgl/engine/exports.js'], function (q, Shadelet, Engine) {
+
+    return Engine.ThresholdShadelet = q.createClass(Shadelet, {
+        
+        format: function () {
+            var glsl = "";
+
+            var node = this.node();
+            
+            var parameter = this.symbolName(node.parameter());
+            var value = node.value();
+            if (typeof value === 'string') {
+                value = this.symbolName(node.value());
+            } else {
+                value = this.formatFloat(node.value());
+            }
+            
+
+            var below = node.below();
+            if (below) {
+                glsl += "vec4 below = " + this.nodeName(node.below()) + ";\n";
+            } else {
+                glsl += "vec4 below = vec4(0.0, 0.0, 0.0, 0.0);\n";
+            }
+
+            var above = node.above();
+            if (above) {
+                glsl += "vec4 above = " + this.nodeName(above) + ";\n";
+            } else {
+                glsl += "vec4 above = vec4(0.0, 0.0, 0.0, 0.0);\n";
+            }
+
+
+            glsl += this.nodeName() + " = mix(below, above, step(" + value + ", " + parameter + "));\n";
+                    
+            return glsl;
+        }
+    });
+});
