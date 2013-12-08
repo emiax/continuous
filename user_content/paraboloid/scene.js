15bf4a177e03328f0b494f33ecf0c214bbe3a8aa
emiax <mail@emilaxelsson.se>
2013-12-08 01:47:41 +0100

    prettified paraboloid

--- user_content/paraboloid/scene.js
+++ user_content/paraboloid/scene.js
@@ -55,11 +55,11 @@ define([
             primitives: {
                 x: 1,
                 y: 0,
-                z: 1
+                z: 4
             },
             expressions: {
                 position: '[x, y, z]',
-                subject: '[0, 0, 0]',
+                subject: '[0, 0, 1]',
                 up: '[0, 0, 1]'
             }
         });
@@ -69,8 +69,8 @@ define([
         var t;
         function update() {
             t = State.t;
-            camera.primitive('x', Math.cos(2*Math.PI * t / 1000) + 0.01);
-            camera.primitive('y', Math.sin(2*Math.PI * t / 1000) + 0.01);
+            camera.primitive('x', 4*Math.cos(2*Math.PI * t / 1000) + 0.01);
+            camera.primitive('y', 4*Math.sin(2*Math.PI * t / 1000) + 0.01);
             ++State.t;
         }
 
@@ -78,27 +78,27 @@ define([
         // surface appearance.
         var red = new MathGL.Color(0xffff0000);
         var green = new MathGL.Color(0xff00cc00);
-        var blue = new MathGL.Color(0xff0000ff);
+        var blue = new MathGL.Color(0xff222299);
+        var cyan = new MathGL.Color(0xff007bbb);
 
         var gradient = new MathGL.Gradient({
             parameter: 'z',
             blendMode: 'normal', 
             stops: {
-                '0.8': red,
-                '0': blue,
-                '-0.8': green
+                '0.8': cyan,
+                '0': blue
             }
         });
 
         var darkOverlay = new MathGL.Color({
-            color: 0x55000000,
+            color: 0x22000000,
             background: gradient
         });
 
         var checker = new MathGL.CheckerPattern({
             parameters: {
-                v: 0.25,
-                u: 0.25
+                v: 0.1,
+                u: 0.1
             },
             inputA: gradient,
             inputB: darkOverlay
@@ -107,8 +107,8 @@ define([
 
         var surface = new MathGL.Surface({
             domain: {
-                u: [-0.5, 0.5],
-                v: [-0.5, 0.5]
+                u: [-1, 1],
+                v: [-1, 1]
             },
             expressions: {
                 x: 'u',
